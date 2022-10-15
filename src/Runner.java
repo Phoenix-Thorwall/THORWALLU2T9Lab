a319eb9d00e92024f5696d30f72246b410f5d696
Phoenix <phoenix.thorwall@gmail.com>
2022-10-15 18:22:51 -0400

    Done With The Lab! I Have Other Homework to do Now <//3

--- src/Runner.java
+++ src/Runner.java
@@ -2,22 +2,6 @@ public class Runner
 {
     public static void main(String[]args)
     {
-    int target = 0;
-    while (target < 25 || target > 100)
-    {
-        target =
-    }
-
-
-
-
-
-        //        int step1 = 0;
-//        while (step1 == 0)
-//        {
-//            step1 = (int) (Math.random() * 10);
-//        }
-//        System.out.println((double) step1);
-
+      System.out.println(Math.ceil(10);
     }
 }
